0a7f02a47dec9cf13dd6bf1cdd878d8a81aaa00e
andrewalson <83783605+andrewalson@users.noreply.github.com>
2024-04-10 18:06:24 -0400

    Spring Initializer configuration and initial commit.

---
+++ src/test/java/com/chessworld/puzzler/PuzzlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chessworld.puzzler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
