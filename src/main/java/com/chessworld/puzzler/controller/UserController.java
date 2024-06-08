763833d972ce2d8e0e9296055458c23ff10f9bf6
andrewalson <83783605+andrewalson@users.noreply.github.com>
2024-06-08 12:01:08 -0400

    Add user button

--- src/main/java/com/chessworld/puzzler/controller/UserController.java
+++ src/main/java/com/chessworld/puzzler/controller/UserController.java
@@ -2,9 +2,11 @@ package com.chessworld.puzzler.controller;
 
 import com.chessworld.puzzler.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController // telling Spring this class will have endpoints
 public class UserController {
 
